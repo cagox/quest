22f237114769315450e670349277fd5e3c1267bc
burlingk <cagox@cagox.net>
2025-03-16 20:23:30 +0900

    Status as of the end of Exercise 1

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 def main():
-    print("Hello World!")
+    hero = Character("Hero", "Adventurer", 35)
 
+    print(hero)
 
-
-
-
-
-
+class Character:
+    def __init__(self, name, character_class, hitpoints):
+        self.name = name
+        self.character_class = character_class
+        self.hitpoints = hitpoints
+    
+    def __repr__(self):
+        return f"{self.name} is a member of the {self.character_class} class, and has {self.hitpoints} hitpoints."
 
 main()
