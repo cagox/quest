3fdc4ca7db2e98dbbf119254105f48a75f8dc804
Cagox <cagox@cagox.net>
2025-04-12 23:20:39 +0900

    Added a few notes in comments in main.py

--- src/main.py
+++ src/main.py
@@ -21,21 +21,15 @@ def main():
         'quintessence': 20,
         'copper': (16*COPPER + 11*SILVER + 2*GOLD + 1*PLATINUM),
     }
-    hero = Character("Hero", **elf)
-    
-    #long_description = "You are in a glorious room full of wonderful stuff. "
-    #long_description += "I am sure this description will go over and have to be wrapped. "
-     
-    
-    #start_room = Room("The Beginning", long_description, "The Starting Room is through that door.")
-    #second_room = Room("The End", "You are in the second room.", "The second room is through that door.")
-    
-    #start_room.add_exit_and_return("north", second_room, "south")
-    # second_room.add_exit("south", start_room)
-    
+    hero = Character("Hero", **elf)   
     game = Game(World(), hero)
     game.world.build_maze()
     decorate(game)
     game.run()
 
 main()
+
+
+# TODO: Have "fold_desciption" check to see if a description is empty.
+# TODO: Have Make sure fold description is folding at the right time. 
+#       I saw an edge case that moved the border.
